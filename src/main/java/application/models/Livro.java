2c76d2113dc1298f6e0ac5e66e1272b52a0a2548
daltonfranco <daltonpacolafranco.git@hotmail.com>
2022-03-29 19:45:31 +0000

    alteracoes

--- src/main/java/application/models/Livro.java
+++ src/main/java/application/models/Livro.java
@@ -1,11 +1,35 @@
-package main.java.application.models;
+package application.models;
+
+import javax.annotation.Generated;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
+@Table(name="Livros")
 
 public class Livro{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titulo;
 
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
 
 }
 
