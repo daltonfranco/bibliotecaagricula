8e7f122a3481394acfe0eee9ef78fac256f66d6c
daltonfranco <daltonpacolafranco.git@hotmail.com>
2022-03-29 18:38:33 +0000

    alteracoes

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,5 +1,12 @@
-package main.java.application;
+package application;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        
+    }
     
 }
